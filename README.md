d6de725f45a23e2d0efa099e536e74d856f53d72
Iurii Ivanov <yuran2016@gmail.com>
2025-02-21 18:32:38 +0100

    finalized tokenizer

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 1. https://craftinginterpreters.com/
 2. https://www.booleanworld.com/building-recursive-descent-parsers-definitive-guide/
-3. Kent D.Lee: Foundations of Programming Languages, Syntax
-4. 
+3. https://tratt.net/laurie/blog/2020/which_parsing_approach.html
+4. https://www.booleanworld.com/building-recursive-descent-parsers-definitive-guide/
+5. 
+
+Kent D.Lee: Foundations of Programming Languages, Syntax
+
+### Beginners guides
+1. https://blog.gopenai.com/building-a-parser-a-beginner-friendly-guide-to-constructing-an-ast-based-parser-37d4a0e782d4
 
 DOT utility example
 https://www.youtube.com/watch?v=tq3zPnrQIpU
--- language_parser/tests/test_tokenizer.py
+++ language_parser/tests/test_tokenizer.py
@@ -1,23 +1,25 @@
 import unittest
 from language_parser.tokenizer import Tokenizer
+from language_parser.tokenizer.consts import (
+    FUNC,
+    IDENTIFIER,
+    PLUS,
+    EQ,
+    NEWLINE,
+    NUMBER,
+    OUT,
+    ASSIGN,
+    IN,
+    BRANCH,
+    LP,
+    MINUS,
+    RP
+)
 from language_parser.tokenizer.tokenizer import Token
 
 
 class TestTokenizer(unittest.TestCase):
-    def test_should_tokenize_sum_function(self):
-        code = '''
-            Munus sum a b = a + b
-            As computo = sum XI C
-            Grafo computo
-        '''
 
-        tokenizer = Tokenizer
-        tokens = tokenizer.tokenize(code)
-
-        # assumption: function name is also lowercase
-        pass
-
-    # should get list of tokens
     def test_should_tokenize(self):
         code = '''
             As uno = XI + C
@@ -30,31 +32,103 @@ class TestTokenizer(unittest.TestCase):
         tokens = tokenizer.tokenize(code)
 
         expected_tokens = [
-            Token("ASSIGN", "As"),
-            Token("ID", "uno"),
-            Token("EQ", "="),
-            Token("NUMBER", "XI"),
-            Token("PLUS", "+"),
-            Token("NUMBER", "C"),
+            Token(ASSIGN, "As"),
+            Token(IDENTIFIER, "uno"),
+            Token(EQ, "="),
+            Token(NUMBER, "XI"),
+            Token(PLUS, "+"),
+            Token(NUMBER, "C"),
+            Token(NEWLINE, "\n"),
+
+            Token(ASSIGN, "As"),
+            Token(IDENTIFIER, "de"),
+            Token(EQ, "="),
+            Token(IN, "Anagnosi"),
+            Token(NEWLINE, "\n"),
+
+            Token(ASSIGN, "As"),
+            Token(IDENTIFIER, "tre"),
+            Token(EQ, "="),
+            Token(IDENTIFIER, "uno"),
+            Token(PLUS, "+"),
+            Token(IDENTIFIER, "de"),
+            Token(NEWLINE, "\n"),
+
+            Token(OUT, "Grafo"),
+            Token(IDENTIFIER, "tre"),
+            Token(NEWLINE, "\n")
+        ]
+
+        self.assertListEqual(tokens, expected_tokens)
+
+    def test_should_tokenize_sum_function(self):
+        code = '''
+            Munus sum a b = a + b
+            As computo = sum XI C
+            Grafo computo
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+
+        expected_tokens = [
+            Token(FUNC, "Munus"),
+            Token(IDENTIFIER, "sum"),
+            Token(IDENTIFIER, "a"),
+            Token(IDENTIFIER, "b"),
+            Token(EQ, "="),
+            Token(IDENTIFIER, "a"),
+            Token(PLUS, "+"),
+            Token(IDENTIFIER, "b"),
+            Token(NEWLINE, "\n"),
+
+            Token(ASSIGN, "As"),
+            Token(IDENTIFIER, "computo"),
+            Token(EQ, "="),
+            Token(IDENTIFIER, "sum"),
+            Token(NUMBER, "XI"),
+            Token(NUMBER, "C"),
             Token("NEWLINE", "\n"),
 
-            Token("ASSIGN", "As"),
-            Token("ID", "de"),
-            Token("EQ", "="),
-            Token("IN", "Anagnosi"),
-            Token("NEWLINE", "\n"),
+            Token(OUT, "Grafo"),
+            Token(IDENTIFIER, "computo"),
+            Token(NEWLINE, "\n"),
+        ]
 
-            Token("ASSIGN", "As"),
-            Token("ID", "tre"),
-            Token("EQ", "="),
-            Token("ID", "uno"),
-            Token("PLUS", "+"),
-            Token("ID", "de"),
-            Token("NEWLINE", "\n"),
+        self.assertListEqual(tokens, expected_tokens)
 
-            Token("OUT", "Grafo"),
-            Token("ID", "tre"),
-            Token("NEWLINE", "\n")
+    def test_should_tokenize_ternary(self):
+        code = '''
+            Munus fib n = Sinon n I ((fib n - I) + (fib n - II))
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+
+        expected_tokens = [
+            Token(FUNC, "Munus"),
+            Token(IDENTIFIER, "fib"),
+            Token(IDENTIFIER, "n"),
+            Token(EQ, "="),
+            Token(BRANCH, "Sinon"),
+            Token(IDENTIFIER, "n"),
+            Token(NUMBER, "I"),
+            Token(LP, "("),
+            Token(LP, "("),
+            Token(IDENTIFIER, "fib"),
+            Token(IDENTIFIER, "n"),
+            Token(MINUS, "-"),
+            Token(NUMBER, "I"),
+            Token(RP, ")"),
+            Token(PLUS, "+"),
+            Token(LP, "("),
+            Token(IDENTIFIER, "fib"),
+            Token(IDENTIFIER, "n"),
+            Token(MINUS, "-"),
+            Token(NUMBER, "II"),
+            Token(RP, ")"),
+            Token(RP, ")"),
+            Token(NEWLINE, "\n"),
         ]
 
         self.assertListEqual(tokens, expected_tokens)
