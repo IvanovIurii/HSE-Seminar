e115a50fe60e5711b3e16509a2872c58163dee83
Iurii Ivanov <yuran2016@gmail.com>
2025-02-25 21:00:25 +0100

    interpreter V1

--- language_parser/interpreter.py
+++ language_parser/interpreter.py
@@ -1,49 +1,170 @@
+import copy
+
+from language_parser.syntax_tree.syntax_tree import parse
+from language_parser.tokenizer import Tokenizer
+from language_parser.tokenizer.consts import NUMBER, IDENTIFIER, FUNC, ASSIGN, OUT, PLUS, MINUS, MULT, DIV
+
+
+def roman_to_int(roman):
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
+    for ch in reversed(roman):
+        value = roman_map.get(ch, 0)
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+            prev_value = value
+
+    return total
+
+
+class Function:
+    def __init__(self, param_names, body):
+        self.param_names = param_names
+        self.body = body
+
+    def __repr__(self):
+        return f"<Function params={self.param_names}>"
+
+
 class Interpreter:
     def __init__(self):
-        self.variables = {}
+        # mapping
+        self.env = {}
+
+    def interpret(self, ast):
+        return self.evaluate(ast)
 
     def evaluate(self, node):
-        if node.type == 'NUMBER':
-            if node.value == 'XI':
-                return 11
+        """
+        Recursively evaluates an AST node.
+        """
+        if node is None:
+            return None
 
-            return roman_to_integer.get(node.value)
+        # --- Statement Level Evaluations ---
+        # Process the root node by evaluating each child (statement) sequentially.
+        if node.type == "ROOT":  # todo: create note type root as well?
+            result = None
+            for child in node.children:
+                result = self.evaluate(child)
 
-        elif node.type == 'ID':
-            return self.variables.get(node.value, 0)
+            return result
 
-        elif node.type == 'PLUS':
-            left_value = self.evaluate(node.children[0])
-            right_value = self.evaluate(node.children[1])
-            return left_value + right_value
+        # If node is a simple token (a leaf without children)
+        if not hasattr(node, 'children'):
+            if node.type == NUMBER:
+                return roman_to_int(node.value)
 
-        elif node.type == 'MINUS':
-            left_value = self.evaluate(node.children[0])
-            right_value = self.evaluate(node.children[1])
-            return left_value - right_value
+            if node.type == IDENTIFIER:
+                if node.value in self.env:
+                    return self.env[node.value]
+                else:
+                    # todo: write a test
+                    raise ValueError(f"Undefined variable '{node.value}'")
 
-        elif node.type == 'ASSIGN':
-            value = self.evaluate(node.children[2])
-            self.variables[node.children[0].value] = value
-            return value
+            return node.value
 
-        # todo: add user input from Terminal
-        elif node.type == 'IN':
-            return 42
+        # Function definition: "Munus ..."
+        if node.type == FUNC:
+            # node.children[0] is the function name node. Its own children (if any) are the parameters.
+            func_name_node = node.children[0]
+            func_name = func_name_node.value
+            arguments = [child.value for child in func_name_node.children] if func_name_node.children else []
+            # The function body is the third child (after  EQ).
+            func_body = node.children[2]
+            self.env[func_name] = Function(arguments, func_body)
 
-        elif node.type == 'OUT':
-            return self.evaluate(node.children[0])
+            return None
 
-        return 0
+        # Assignment: "As <identifier> '=' <expression>"
+        if node.type == ASSIGN:
+            var_name_node = node.children[0]
+            var_name = var_name_node.value
+            expr = node.children[2]
+            result = self.evaluate(expr)
+            self.env[var_name] = result
+
+            return result
+
+        # Output: "Grafo ..."
+        if node.type == OUT:
+            var_name_node = node.children[0]
+            var_name = var_name_node.value
+            if var_name in self.env:
+                result = self.env[var_name]
+            else:
+                raise ValueError(f"Undefined variable '{var_name}'")
+
+            print(result)
+            return result
+
+        # --- Expression Evaluations ---
+        # Binary arithmetic operators: PLUS, MINUS, MULT, DIV
+        if node.type in (PLUS, MINUS, MULT, DIV):
+            left = self.evaluate(node.children[0])
+            right = self.evaluate(node.children[1])
+
+            if node.type == PLUS:
+                return left + right
+            elif node.type == MINUS:
+                return left - right
+            elif node.type == MULT:
+                return left * right
+            elif node.type == DIV:
+                return left // right if isinstance(left, int) and isinstance(right, int) else left / right
+
+        # Branch operator: simply evaluate its child expression.
+        # if node_type == BRANCH:
+        #     # fixme: there is a bug currently, in Sinon 1rt arg is treated as function,
+        #     #  we should keep the context that it is inside the branch
+        #     return self.evaluate(node.children[0], env)
+
+        # Function call:
+        # If an IDENTIFIER node has children, we treat it as a function call.
+        # todo: check with Sinon n <expr>
+        if node.type == IDENTIFIER and node.children:
+            func = self.env.get(node.value)
+            if func is None:
+                raise ValueError(f"Undefined function '{node.value}'")
+            if not isinstance(func, Function):
+                raise ValueError(f"'{node.value}' is not callable")
+            # Evaluate each argument.
+            args = [self.evaluate(child) for child in node.children]
+            if len(args) != len(func.param_names):
+                raise ValueError(f"Function '{node.value}' expects {len(func.param_names)} arguments, got {len(args)}")
+
+            for param, arg in zip(func.param_names, args):
+                self.env[param] = arg
+
+            return self.evaluate(func.body)
+
+        # Variable reference: simple identifier with no children.
+        if node.type == IDENTIFIER:
+            if node.value in self.env:
+                return self.env[node.value]
+            else:
+                raise ValueError(f"Undefined variable '{node.value}'")
+
+        # Number literal.
+        if node.type == NUMBER:
+            return roman_to_int(node.value)
+
+        return node.value
 
 
-# todo: write a function to convert Roman numbers to integers
-roman_to_integer = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
+if __name__ == '__main__':
+    code = """
+        Munus sum a b c = a + b + c
+        As computo = sum XI C I
+        Grafo computo
+        """
+
+    tokens = Tokenizer.tokenize(code)
+    ast_root = parse(tokens)
+
+    interpreter = Interpreter()
+    interpreter.interpret(ast_root)
---
+++ language_parser/tests/test_interpreter.py
@@ -0,0 +1,180 @@
+import unittest
+
+from language_parser.interpreter import Interpreter
+from language_parser.syntax_tree.syntax_tree import parse
+from language_parser.tokenizer import Tokenizer
+
+
+class TestInterpreter(unittest.TestCase):
+    def test_should_return_21(self):
+        code = '''
+            Munus sum a b c = a + b + c
+            As computo = sum X X I
+            Grafo computo
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        interpreter = Interpreter()
+        result = interpreter.interpret(ast)
+
+        self.assertEquals(result, 21)
+
+    def test_should_return_an_error(self):
+        code = '''
+            Munus sum a b c = a + b + c
+            As computo = sum X X
+            Grafo computo
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        interpreter = Interpreter()
+
+        # todo: read doc how to use this
+        with self.assertRaises(ValueError) as cm:
+            interpreter.interpret(ast)
+
+        the_exception = cm.exception
+        self.assertEqual(the_exception.args[0], "Function 'sum' expects 3 arguments, got 2")
+
+    # NOTE: this is for the sake of testing, we get a result of latest statement as value
+    def test_should_add(self):
+        code = '''
+            As uno = XI + C
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        interpreter = Interpreter()
+        result = interpreter.interpret(ast)
+
+        self.assertEqual(result, 111)
+
+    def test_should_subtract(self):
+        code = '''
+            As uno = C - I
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        interpreter = Interpreter()
+        result = interpreter.interpret(ast)
+
+        self.assertEqual(result, 99)
+
+    def test_should_return_6(self):
+        code = '''
+            As uno = V - II + III
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        interpreter = Interpreter()
+        result = interpreter.interpret(ast)
+
+        self.assertEquals(result, 6)
+
+    def test_should_return_0(self):
+        code = '''
+            As uno = V - (II + III)
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        interpreter = Interpreter()
+        result = interpreter.interpret(ast)
+
+        self.assertEquals(result, 0)
+
+    def test_should_build_ast_for_function_invocation_sum_first(self):
+        code = '''
+            As duo = sum a (I + XI)
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        interpreter = Interpreter()
+        result = interpreter.interpret(ast)
+
+        pass
+
+    def test_should_build_ast_for_function_invocation_with_op_and_parenthesis(self):
+        code = '''
+            As tres = sum XI (D + L)
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        ast.print_recursively()
+
+    def test_should_build_ast_for_ops_with_parenthesis(self):
+        code = '''
+            As quatro = XI + (D - L)
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        ast.print_recursively()
+
+    def test_should_build_ast_for_function_inside_with(self):
+        code = '''
+            As minimum = XI + (min a b)
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        ast.print_recursively()
+
+    def test_should_assign_one_roman_number(self):
+        code = '''
+            As uno = X
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        ast.print_recursively()
+
+    def test_should_build_ast_for_ternary_operator_statement(self):
+        code = '''
+            Munus fib n = Sinon n I ((fib (n - I)) + (fib (n - II)))
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        ast.to_mermaid_markdown("Sinon")
+
+    def test_foo(self):
+        code = '''
+            As foo = fib (n - I)
+        '''
+
+        tokenizer = Tokenizer
+        tokens = tokenizer.tokenize(code)
+        ast = parse(tokens)
+
+        ast.to_mermaid_markdown("Assign fib")
